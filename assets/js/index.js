f01d25cfb9dbfd966bc068a615e1724cdfe105fd
Bibiwei Pere <bibiweipere@gmail.com>
2023-06-19 08:05:26 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,38 @@
-const mark = document.getElementById("mark");
-const angela = document.getElementById("angela");
-const jacob = document.getElementById("jacob");
-const button = document.getElementById("request");
+const markRead = document.querySelector('.mark');
+const unreadCircles = document.querySelectorAll('.unread-dot');
+const notificationCount = document.querySelector('.notification-count');
+const notificationElements = document.querySelectorAll('.box');
+const unreadElements = document.querySelectorAll('.unread');
+const title = document.querySelectorAll('.title');
 
-button.addEventListener("click", getinput);
+// Update the notification count text with the number of unread notifications
+notificationCount.innerHTML = `<p class="title">Notifications</p><div class="number">${unreadElements.length}</div>`;
 
-function getinput(){
-    mark.style.borderColor ='hsl(0, 0%, 86%)'
-    angela.style.borderColor ='hsl(0, 0%, 86%)'
-    jacob.style.borderColor ='hsl(0, 0%, 86%)'
+//Hide unread elements on click of "Mark all as read" button
+markRead.addEventListener('click', () => {
+  unreadCircles.forEach((div) => {
+    div.style.display = 'none';
+  });
+  document.querySelectorAll('.box.unread').forEach(box => {
+    box.classList.remove('unread');
+  }); 
+  //when button is clicked, notification number resets to 0
+  notificationCount.innerHTML = `<p class="title">Notification</p><div class="number">0</div>`;
+});
 
-    const myLabel = document.querySelector('mark');
-    const myLabe2 = document.querySelector('angela');
-    const myLabe3 = document.querySelector('jacob');
+// Add event listener to each box element
+notificationElements.forEach((box) => {
+  box.addEventListener('click', () => {
+    // Remove the unread class from the clicked box element
+    box.classList.remove('unread');
 
-    myLabel.style.color = 'hsl(0, 0%, 8%)'
-    myLabe2.style.color = 'hsl(0, 0%, 8%)'
-    myLabe3.style.color = 'hsl(0, 100%, 8%)'
-}
+    //Get rid of unread circle when box is clicked
+    const unreadCircle = box.querySelector('.unread-dot');
+    if (unreadCircle) {
+      unreadCircle.remove();
+    }
+    // Update the notification count text
+    const unreadElement = document.querySelectorAll('.unread');
+      notificationCount.innerHTML = `<p class="title">Notifications</p><div class="number">${unreadElement.length}</div>`;
+  });
+});
